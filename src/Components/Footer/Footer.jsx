93a5a406323c1605b252c8d3f0a0a742952e4dfa
MirRizwan <mir_rizwan@yahoo.com>
2020-06-20 18:19:56 +0400

    First bushirts layout. Facing JQuery problems. 20062020

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,137 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <footer>
+	<div className="tt-footer-col tt-color-scheme-01">
+		<div className="container">
+			<div className="row">
+				<div className="col-md-6 col-lg-2 col-xl-3">
+					<div className="tt-mobile-collapse">
+						<h4 className="tt-collapse-title">
+							CATEGORIES
+						</h4>
+						<div className="tt-collapse-content">
+							<ul className="tt-list">
+								<li><a href="listing-collection.html">Women</a></li>
+								<li><a href="listing-collection.html">Men</a></li>
+								<li><a href="listing-collection.html">Accessories</a></li>
+								<li><a href="listing-collection.html">Shoes</a></li>
+								<li><a href="listing-collection.html">New Arrivals</a></li>
+								<li><a href="listing-collection.html">Clearence</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div className="col-md-6 col-lg-2 col-xl-3">
+					<div className="tt-mobile-collapse">
+						<h4 className="tt-collapse-title">
+							MY ACCOUNT
+						</h4>
+						<div className="tt-collapse-content">
+							<ul className="tt-list">
+								<li><a href="account_order.html">Orders</a></li>
+								<li><a href="page404.html">Compare</a></li>
+								<li><a href="page404.html">Wishlist</a></li>
+								<li><a href="login.html">Log In</a></li>
+								<li><a href="create-account.html">Register</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div className="col-md-6 col-lg-4 col-xl-3">
+					<div className="tt-mobile-collapse">
+						<h4 className="tt-collapse-title">
+							CONTACTS
+						</h4>
+						<div className="tt-collapse-content">
+							<address>
+								<p><span>Address:</span> 2548 Broaddus Maple Court Avenue, Madisonville KY 4783, United States of America</p>
+								<p><span>Phone:</span> +777 2345 7885;  +777 2345 7886</p>
+								<p><span>Hours:</span> 7 Days a week from 10 am to 6 pm</p>
+								<p><span>E-mail:</span> <a href="mailto:info@mydomain.com">info@mydomain.com</a></p>
+							</address>
+						</div>
+					</div>
+				</div>
+				<div className="col-md-6 col-lg-4 col-xl-3">
+					<div className="tt-newsletter">
+						<div className="tt-mobile-collapse">
+							<h4 className="tt-collapse-title">
+								NEWSLETTER SIGNUP
+							</h4>
+							<div className="tt-collapse-content">
+								<p>
+									Sign up for our e-mail and be the first who know our special offers! Furthermore, we will give a 15% discount on the next order after you sign up.
+								</p>
+								<form id="newsletterform" className="form-inline form-default" method="post" novalidate="novalidate" action="#">
+									<div className="form-group">
+										<input type="text" name="email" className="form-control" placeholder="Enter your e-mail" />
+										<button type="submit" className="btn">JOIN US</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+					<ul className="tt-social-icon">
+						<li><a className="icon-g-64" target="_blank" href="http://www.facebook.com/"></a></li>
+						<li><a className="icon-h-58" target="_blank" href="http://www.facebook.com/"></a></li>
+						<li><a className="icon-g-66" target="_blank" href="http://www.twitter.com/"></a></li>
+						<li><a className="icon-g-67" target="_blank" href="http://www.google.com/"></a></li>
+						<li><a className="icon-g-70" target="_blank" href="https://instagram.com/"></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div className="tt-footer-custom">
+		<div className="container">
+			<div className="tt-row">
+				<div className="tt-col-left">
+					<div className="tt-col-item tt-logo-col">
+						
+						<a className="tt-logo tt-logo-alignment" href="index.html">
+							<img src="assets/images/custom/logo.png" alt="" />
+						</a>
+						
+					</div>
+					<div className="tt-col-item">
+					
+						<div className="tt-box-copyright">
+							&copy; Wokiee 2018. All Rights Reserved
+						</div>
+						
+					</div>
+				</div>
+				<div className="tt-col-right">
+					<div className="tt-col-item">
+					
+						<ul className="tt-payment-list">
+							<li><a href="page404.html"><span className="icon-Stripe"><span className="path1"></span><span className="path2"></span><span className="path3"></span><span className="path4"></span><span className="path5"></span><span className="path6"></span><span className="path7"></span><span className="path8"></span><span className="path9"></span><span className="path10"></span><span className="path11"></span><span className="path12"></span>
+			                </span></a></li>
+							<li><a href="page404.html"> <span className="icon-paypal-2">
+			                <span className="path1"></span><span className="path2"></span><span className="path3"></span><span className="path4"></span><span className="path5"></span><span className="path6"></span>
+			                </span></a></li>
+							<li><a href="page404.html"><span className="icon-visa">
+			                <span className="path1"></span><span className="path2"></span><span className="path3"></span><span className="path4"></span><span className="path5"></span>
+			                </span></a></li>
+							<li><a href="page404.html"><span className="icon-mastercard">
+			                <span className="path1"></span><span className="path2"></span><span className="path3"></span><span className="path4"></span><span className="path5"></span><span className="path6"></span><span className="path7"></span><span className="path8"></span><span className="path9"></span><span className="path10"></span><span className="path11"></span><span className="path12"></span><span className="path13"></span>
+			                </span></a></li>
+							<li><a href="page404.html"><span className="icon-discover">
+			                <span className="path1"></span><span className="path2"></span><span className="path3"></span><span className="path4"></span><span className="path5"></span><span className="path6"></span><span className="path7"></span><span className="path8"></span><span className="path9"></span><span className="path10"></span><span className="path11"></span><span className="path12"></span><span className="path13"></span><span className="path14"></span><span className="path15"></span><span className="path16"></span>
+			                </span></a></li>
+							<li><a href="page404.html"><span className="icon-american-express">
+			                <span className="path1"></span><span className="path2"></span><span className="path3"></span><span className="path4"></span><span className="path5"></span><span className="path6"></span><span className="path7"></span><span className="path8"></span><span className="path9"></span><span className="path10"></span><span className="path11"></span>
+			                </span></a></li>
+						</ul>
+					
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+    )
+}
+export default Footer;
