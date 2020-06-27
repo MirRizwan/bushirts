5952cc7d7e5fa9b20e4ad5636afc3adfefa1885f
MirRizwan <mir_rizwan@yahoo.com>
2020-06-27 12:41:38 +0400

    Change Menu and seperate remaining components

---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const SearchBar = () => {
+    return(
+        <div className="tt-desctop-parent-search tt-parent-box">
+						<div className="tt-search tt-dropdown-obj">
+							<button className="tt-dropdown-toggle" data-tooltip="Search" data-tposition="bottom">
+								<i className="icon-f-85"></i>
+							</button>
+							<div className="tt-dropdown-menu">
+								<div className="container">
+									<form>
+										<div className="tt-col">
+											<input type="text" readOnly className="tt-search-input" placeholder="Search Products..." />
+											<button className="tt-btn-search" type="submit"></button>
+										</div>
+										<div className="tt-col">
+											<button className="tt-btn-close icon-g-80"></button>
+										</div>
+										<div className="tt-info-text">
+											What are you Looking for?
+										</div>
+										<div className="search-results">
+											<ul>
+												<li>
+										            <Link to="product.html">
+										            	<div className="thumbnail"><img src="assets/images/loader.svg" data-src="assets/images/product/product-03.jpg" alt="" /></div>
+										            	<div className="tt-description">
+										            		<div className="tt-title">Flared Shift Bag</div>
+										            		<div className="tt-price">
+																<span className="new-price">$14</span>
+																<span className="old-price">$24</span>
+															</div>
+										            	</div>
+										            </Link>
+										        </li>
+										        <li>
+										           <Link to="product.html">
+										            	<div className="thumbnail"><img src="assets/images/loader.svg" data-src="assets/images/product/product-02.jpg" alt="" /></div>
+										            	<div className="tt-description">
+										            		<div className="tt-title">Flared Shift Bag</div>
+										            		<div className="tt-price">
+																$24
+															</div>
+										            	</div>
+										            </Link>
+										        </li>
+										        <li>
+										           <Link to="product.html">
+										            	<div className="thumbnail"><img src="assets/images/loader.svg" data-src="assets/images/product/product-01.jpg" alt="" /></div>
+										            	<div className="tt-description">
+										            		<div className="tt-title">Flared Shift Bag</div>
+										            		<div className="tt-price">
+																$14
+															</div>
+										            	</div>
+										            </Link>
+										        </li>
+										        <li>
+										           <Link to="product.html">
+										            	<div className="thumbnail"><img src="assets/images/loader.svg" data-src="assets/images/product/product-04.jpg" alt="" /></div>
+										            	<div className="tt-description">
+										            		<div className="tt-title">Flared Shift Bag</div>
+										            		<div className="tt-price">
+																$24
+															</div>
+										            	</div>
+										            </Link>
+										        </li>
+										        <li>
+										           <Link to="product.html">
+										            	<div className="thumbnail"><img src="assets/images/loader.svg" data-src="assets/images/product/product-05.jpg" alt="" /></div>
+										            	<div className="tt-description">
+										            		<div className="tt-title">Flared Shift Bag</div>
+										            		<div className="tt-price">
+																$17
+															</div>
+										            	</div>
+										            </Link>
+										        </li>
+										        <li>
+										           <Link to="product.html">
+										            	<div className="thumbnail"><img src="assets/images/loader.svg" data-src="assets/images/product/product-06.jpg" alt="" /></div>
+										            	<div className="tt-description">
+										            		<div className="tt-title">Flared Shift Bag</div>
+										            		<div className="tt-price">
+																$20
+															</div>
+										            	</div>
+										            </Link>
+										        </li>
+											</ul>
+											<button type="button" className="tt-view-all">View all products</button>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+    )
+}
+
+export default SearchBar;
---
+++ src/Components/UserOptions/UserOptions.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const UserOptions = () => {
+    return(
+        <div className="tt-desctop-parent-account tt-parent-box">
+            <div className="tt-account tt-dropdown-obj">
+                <button className="tt-dropdown-toggle"  data-tooltip="My Account" data-tposition="bottom"><i className="icon-f-94"></i></button>
+                <div className="tt-dropdown-menu">
+                    <div className="tt-mobile-add">
+                        <button className="tt-close">Close</button>
+                    </div>
+                    <div className="tt-dropdown-inner">
+                        <ul>
+                            <li><Link to="login.html"><i className="icon-f-94"></i>Account</Link></li>
+                            <li><Link to="wishlist.html"><i className="icon-n-072"></i>Wishlist</Link></li>
+                            <li><Link to="compare.html"><i className="icon-n-08"></i>Compare</Link></li>
+                            <li><Link to="page404.html"><i className="icon-f-68"></i>Check Out</Link></li>
+                            <li><Link to="login.html"><i className="icon-f-76"></i>Sign In</Link></li>
+                            <li><Link to="page404.html"><i className="icon-f-77"></i>Sign Out</Link></li>
+                            <li><Link to="create-account.html"><i className="icon-f-94"></i>Register</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserOptions;
